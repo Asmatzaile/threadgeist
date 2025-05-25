771b526112beaf948e5777fbf882310d55cf844f
Asmatzaile <gorkainventor@gmail.com>
2025-05-25 13:41:31 +0200

    accept multiple images

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { createDrawer, imageLoader, stippler, threader } from "./index"
 import { CreatePathModal } from "./CreatePathModal";
 import { CreateStippleModal } from "./CreateStippleModal";
@@ -9,20 +9,27 @@ function App() {
     const createPathModalRef = useRef();
     const createStippleModalRef = useRef();
 
+    const [imageUrls, setImageUrls] = useState();
+    const [currentImageIndex, setCurrentImageIndex] = useState();
     useEffect(() => {
         createDrawer(canvasRef.current);
     }, [])
 
     const forceUpdate = useForceUpdate();
-    function uploadImage(file) {
-        const blobURL = URL.createObjectURL(file);
-        imageLoader.load(blobURL, forceUpdate);
+    function uploadImages(files) {
+        setImageUrls(Array.from(files).map(URL.createObjectURL))
+        setCurrentImageIndex(0);
     }
+    useEffect(()=> {
+        if (currentImageIndex === undefined) return;
+        imageLoader.load(imageUrls[currentImageIndex], forceUpdate)
+    }, [currentImageIndex, imageUrls]);
+
     return <>
         <h1>Threadgeist</h1>
         <label className="relative">
-            <button className="px-4 border-2">Upload image</button>
-            <input type="file" accept="image/*" multiple={false} className="opacity-0 cursor-pointer absolute inset-0 w-full h-full bg-sky-300" onChange={(e) => uploadImage(e.target.files[0])}/>
+            <button className="px-4 border-2">Upload image(s)</button>
+            <input type="file" accept="image/*" multiple={true} className="opacity-0 cursor-pointer absolute inset-0 w-full h-full" onChange={(e) => uploadImages(e.target.files)}/>
         </label>
         <canvas hidden={!imageLoader.status === 'unstarted'} ref={canvasRef} />
         
@@ -41,6 +48,12 @@ function App() {
                     {threader.status === 'working' && <button className="px-4 border-2 cursor-pointer" onClick={() => threader.stop()}>Stop path generation</button>}
                 </div>
             }
+        </>}
+
+        { imageUrls && imageUrls.length > 1 && <>
+            <button disabled={currentImageIndex===0} onClick={() => setCurrentImageIndex(p=>p-1)} className="px-4 border-2 enabled:cursor-pointer disabled:opacity-50">Previous</button>
+            <span>Image {currentImageIndex+1} of {imageUrls.length}</span>
+            <button disabled={currentImageIndex===imageUrls.length-1} onClick={() => setCurrentImageIndex(p=>p+1)}className="px-4 border-2 enabled:cursor-pointer disabled:opacity-50">Next</button>
         </>}
 
         <CreateStippleModal ref={createStippleModalRef} forceUpdate={forceUpdate}/>
